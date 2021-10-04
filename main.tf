930fbfd0ad8ba6fbfb8fe233d0b4e82b1fe76a2d
syed4tech <73010358+syed4tech@users.noreply.github.com>
2021-10-04 18:32:11 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region     = "us-west-1"
-  access_key = "AKIA3JQCPV4KIO367HGY"
-  secret_key = "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"
+  access_key = "AKIA3JQCPV4KEHWNZVNE"
+  secret_key = "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"
 }
 resource "aws_s3_bucket" "mybucket" {
 	bucket = "mybestb3456ucket1"
