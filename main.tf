26a64c6bbb6c0f0154b4d86e79c34449a7f27853
syed4tech <73010358+syed4tech@users.noreply.github.com>
2021-10-07 08:03:39 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,9 @@
 provider "aws" {
   region     = "us-west-1"
-  access_key = "AKIA3JQCPV4KEHWNZVNE"
-  secret_key = "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"
+  access_key = "AKIA3JQCPV4KPJCGODWK"
+  secret_key = "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"
 }
 resource "aws_s3_bucket" "mybucket" {
 	bucket = "mybestb3456ucket1"
+	acl = "private"
 }
