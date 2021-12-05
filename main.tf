c02484fbb4f54344bef7a24e297512e1f9729499
amrutajabe <85388710+amrutajabe@users.noreply.github.com>
2021-12-05 17:18:24 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region     = "us-west-1"
-  access_key = "AKIA3JQCPV4KMSWW6PFO"
-  secret_key = "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"
+  access_key = "AKIA5ZYFB5V63XG4OWEC"
+  secret_key = "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"
 }
 resource "aws_s3_bucket" "mybucket" {
 	bucket = "mybestb3456ucket1"
